75451ee75e8863c9859429ac791212621bce8dc2
Vibesx <lucian.paslariu@cgm.com>
2018-02-22 10:47:50 +0200

    first commit

---
+++ src/main/java/com/shoptimize/shoptimize/ShoptimizeApplication.java
@@ -0,0 +1,12 @@
+package com.shoptimize.shoptimize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoptimizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoptimizeApplication.class, args);
+	}
+}
